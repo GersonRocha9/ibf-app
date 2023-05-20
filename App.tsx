2c9f30e1649bff8c79f92a2237011897f890742b
Gerson Rocha <gersonrocha9@gmail.com>
2023-05-20 13:16:59 -0300

    feature: create home page

--- App.tsx
+++ App.tsx
@@ -3,9 +3,33 @@ import {
   Poppins_700Bold,
   useFonts,
 } from '@expo-google-fonts/poppins'
-import { Text, View } from 'react-native'
+import { Feather, Ionicons } from '@expo/vector-icons'
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 
 import { StatusBar } from 'expo-status-bar'
+import IBFLogo from './src/assets/logo.png'
+
+const mockedEvents = [
+  {
+    id: 1,
+    title: 'Evento 01',
+  },
+  {
+    id: 2,
+    title: 'Evento 02',
+  },
+  {
+    id: 3,
+    title: 'Evento 03',
+  },
+]
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -18,15 +42,73 @@ export default function App() {
   }
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950 p-5">
-      <Text className="text-white text-4xl font-title">Hello World</Text>
-      <Text className="text-white text-base font-body mt-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eius
-        natus eos temporibus debitis perspiciatis quas praesentium consectetur
-        vitae voluptatibus laboriosam, delectus repellat numquam illo? Ex, iste
-        perferendis? Quisquam, nemo.
+    <ScrollView className="flex-1 bg-green-100 p-5">
+      {/* Logo da IBF */}
+      <Image
+        source={IBFLogo}
+        alt="Logo da Igreja Batista do Flamboyant"
+        style={{ resizeMode: 'contain' }}
+        className="w-52 h-52 self-center"
+      />
+
+      {/* Mensagem de boas vindas */}
+      <Text className="text-green-900 text-2xl font-title text-center">
+        Seja bem vindo(a) a Igreja Batista do Flamboyant!
       </Text>
-      <StatusBar style="auto" />
-    </View>
+
+      {/* Banner do Evento 01 */}
+      <View className="bg-gray-200 rounded-lg mt-5 h-40 justify-center items-center">
+        <Text className="font-body text-base">Evento Principal</Text>
+      </View>
+
+      {/* FlatList na horizontal com próximos eventos */}
+      <View className="flex-row items-center gap-1 mt-5">
+        <Feather name="calendar" size={24} color="#CC93AD" />
+        <Text className="font-title text-lg text-gray-900">
+          Próximos eventos:
+        </Text>
+      </View>
+
+      <FlatList
+        data={mockedEvents}
+        keyExtractor={(item) => String(item.id)}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        ItemSeparatorComponent={() => <View className="w-2" />}
+        renderItem={({ item }) => (
+          <View className="bg-gray-200 rounded-lg mt-2 w-40 h-40 justify-center items-center">
+            <Text className="font-body text-base">{item.title}</Text>
+          </View>
+        )}
+      />
+      <TouchableOpacity className="mt-2">
+        <Text className="font-title text-gray-900">Ver todos</Text>
+      </TouchableOpacity>
+
+      {/* FlatList na horizontal com as pastorais */}
+      <View className="flex-row items-center gap-1 mt-5">
+        <Ionicons name="reader-outline" size={24} color="#CC93AD" />
+        <Text className="font-title text-lg text-gray-900">Pastorais</Text>
+      </View>
+
+      <FlatList
+        data={mockedEvents}
+        keyExtractor={(item) => String(item.id)}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        ItemSeparatorComponent={() => <View className="w-2" />}
+        renderItem={({ item }) => (
+          <View className="bg-gray-200 rounded-lg mt-2 w-40 h-40 justify-center items-center">
+            <Text className="font-body text-base">{item.title}</Text>
+          </View>
+        )}
+      />
+
+      <TouchableOpacity className="mt-2">
+        <Text className="font-title text-gray-900">Ver todos</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="dark" />
+    </ScrollView>
   )
 }
