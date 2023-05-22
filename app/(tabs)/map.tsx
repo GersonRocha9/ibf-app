a23ca68ecb1ff30183303f900733ce7ca1a5ba44
Gerson Rocha <gersonrocha9@gmail.com>
2023-05-22 16:20:30 -0300

    feature: map screen with mapView

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,9 +1,53 @@
 import { Text, View } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export default function Map() {
+  const { top, bottom } = useSafeAreaInsets()
+  const latitude = -21.767511293640347
+  const longitude = -41.3061041505967
+
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-white">Map</Text>
+    <View
+      className="bg-gray-50 flex-1 px-5"
+      style={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <Text className="text-gray-950 text-xl font-title mt-2">
+        Onde estamos?
+      </Text>
+
+      <Text className="text-gray-950 text-sm font-body mt-5">
+        Estamos localizados na Rua Caldas Viana, 250 - Campos dos Goytacazes -
+        Rio de Janeiro
+      </Text>
+
+      <MapView
+        className="w-full h-96 mt-5"
+        initialRegion={{
+          latitude,
+          longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude,
+            longitude,
+          }}
+          title="Igreja Batista do Flamboyant"
+          description="Um lugar de paz e comunhão em Jesus Cristo."
+        />
+      </MapView>
+
+      <Text className="text-gray-950 text-xl font-title text-center mt-5">
+        Venha nos fazer uma visita!
+      </Text>
+
+      <Text className="text-gray-950 text-sm font-body mt-5">
+        Cultos presenciais: Domingos às 10h, 18h e 20h | Quartas às 20h
+      </Text>
     </View>
   )
 }
